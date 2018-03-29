51798cb063eeb8d5887e1ec08f6bc5bb988e0009
Rodrigo Leal <ziongh@hotmail.com>
2018-03-29 18:27:09 -0300

    Added mediatr and simple injector to unit test

--- Akrual.DDD.Utils.Domain.Tests/Domain/CommandEventTests.cs
+++ Akrual.DDD.Utils.Domain.Tests/Domain/CommandEventTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,10 +13,96 @@ using Akrual.DDD.Utils.Domain.Messaging.DomainCommands;
 using Akrual.DDD.Utils.Domain.Messaging.DomainEvents;
 using Akrual.DDD.Utils.Domain.Tests.ExampleDomain;
 using Akrual.DDD.Utils.Domain.Utils.UUID;
+using MediatR;
+using MediatR.Pipeline;
+using SimpleInjector;
 using Xunit;
 
 namespace Akrual.DDD.Utils.Domain.Tests.Domain
 {
+
+
+
+    public class CommandEventTestsWithSimpleInjector : BaseAggregateRootTests<TabAggregate, TabAggregate>
+    {
+        [Fact]
+        public async Task CanOpenANewTab()
+        {
+            var container = new Container();
+
+            container.RegisterSingleton<IMediator, Mediator>();
+            
+            container.Register(typeof(IRequestHandler<,>), AppDomain.CurrentDomain.GetAssemblies());
+            container.Register(typeof(IRequestHandler<>), AppDomain.CurrentDomain.GetAssemblies());
+
+            
+            // we have to do this because by default, generic type definitions (such as the Constrained Notification Handler) won't be registered
+            var notificationHandlerTypes = container.GetTypesToRegister(typeof(INotificationHandler<>), AppDomain.CurrentDomain.GetAssemblies(), new TypesToRegisterOptions
+            {
+                IncludeGenericTypeDefinitions = true,
+                IncludeComposites = false,
+            });
+            container.RegisterCollection(typeof(INotificationHandler<>), notificationHandlerTypes);
+            
+            //Pipeline
+            container.RegisterCollection(typeof(IPipelineBehavior<,>), new []
+            {
+                typeof(RequestPreProcessorBehavior<,>),
+                typeof(RequestPostProcessorBehavior<,>),
+                typeof(GenericPipelineBehavior<,>)
+            });
+            
+            container.RegisterCollection(typeof(IRequestPreProcessor<>), new [] { typeof(GenericRequestPreProcessor<>) });
+            container.RegisterCollection(typeof(IRequestPostProcessor<,>), new[] { typeof(GenericRequestPostProcessor<,>), typeof(ConstrainedRequestPostProcessor<,>) });
+
+
+            container.RegisterSingleton(new SingleInstanceFactory(container.GetInstance));
+            container.RegisterSingleton(new MultiInstanceFactory(container.GetAllInstances));
+
+
+            container.Register(typeof(IHandleDomainEvent<>),
+                AppDomain.CurrentDomain.GetAssemblies());
+
+            container.Register(typeof(IHandleDomainCommand<>),
+                AppDomain.CurrentDomain.GetAssemblies());
+
+            var handler = container.GetInstance<IHandleDomainCommand<OpenTab>>();
+
+            var mediator = container.GetInstance<IMediator>();
+
+
+
+            
+
+            var testId = Guid.NewGuid();
+            var testTable = 42;
+            var testWaiter = "Derek";
+
+            var response = await mediator.Send(new OpenTab(testId)
+            {
+                TableNumber = testTable,
+                Waiter = testWaiter
+            });
+
+            Assert.Equal(1, response.Count());
+
+            Test(new TabAggregate(),
+                Given(),
+                When(new OpenTab(testId)
+                {
+                    TableNumber = testTable,
+                    Waiter = testWaiter
+                }),
+                Then(new TabOpened(testId)
+                {
+                    TableNumber = testTable,
+                    Waiter = testWaiter
+                }));
+        }
+
+    }
+
+
     public class CommandEventTests : BaseAggregateRootTests<TabAggregate, TabAggregate>
     {
         [Fact]
@@ -24,7 +112,7 @@ namespace Akrual.DDD.Utils.Domain.Tests.Domain
             var testTable = 42;
             var testWaiter = "Derek";
 
-            Test(new TabAggregate(testId), 
+            Test(new TabAggregate(), 
                 Given(),
                 When(new OpenTab(testId)
                 {
@@ -45,7 +133,7 @@ namespace Akrual.DDD.Utils.Domain.Tests.Domain
             var testTable = 42;
             var testWaiter = "Derek";
 
-            Test(new TabAggregate(testId), 
+            Test(new TabAggregate(), 
                 Given(new TabOpened(testId)
                 {
                     TableNumber = testTable,
@@ -115,11 +203,6 @@ namespace Akrual.DDD.Utils.Domain.Tests.Domain
         public string Waiter { get; private set; }
         public bool Opened { get; set; }
 
-        public TabAggregate(Guid id) : base(id)
-        {
-            Opened = false;
-        }
-
         public TabAggregate() : base(Guid.Empty)
         {
         }
@@ -140,6 +223,62 @@ namespace Akrual.DDD.Utils.Domain.Tests.Domain
         public async Task Handle(TabOpened notification, CancellationToken cancellationToken)
         {
             Opened = true;
+        }
+    }
+
+
+
+    public class GenericRequestPreProcessor<TRequest> : IRequestPreProcessor<TRequest>
+    {
+
+        public GenericRequestPreProcessor()
+        {
+        }
+
+        public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+
+    public class GenericRequestPostProcessor<TRequest, TResponse> : IRequestPostProcessor<TRequest, TResponse>
+    {
+        public GenericRequestPostProcessor()
+        {
+        }
+
+        public Task Process(TRequest request, TResponse response)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class ConstrainedRequestPostProcessor<TRequest, TResponse>
+        : IRequestPostProcessor<TRequest, TResponse>
+    {
+
+        public ConstrainedRequestPostProcessor()
+        {
+        }
+
+        public Task Process(TRequest request, TResponse response)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+
+    public class GenericPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        public GenericPipelineBehavior()
+        {
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var response = await next();
+            return response;
         }
     }
 }
