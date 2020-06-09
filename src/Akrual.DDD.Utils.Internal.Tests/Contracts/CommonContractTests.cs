192bfe1c4a824c487c9cda6a909e314ed240e447
Rodrigo Leal <akrualtech@outlook.com>
2020-06-09 15:43:50 -0300

    fixed names

---
+++ src/Akrual.DDD.Utils.Internal.Tests/Contracts/CommonContractTests.cs
@@ -0,0 +1,16 @@
+﻿using Akrual.DDD.Utils.Internal.Contracts;
+using Akrual.DDD.Utils.Internal.Exceptions;
+using Akrual.DDD.Utils.Internal.Tests;
+using Xunit;
+
+namespace Akrual.DDD.Utils.Internal.Tests.Contracts {
+    public class CommonContractTests : BaseTests {
+        [Fact]
+        public void EnsureNotNull_WhenNull_ReturnException () {
+            TestingClass variable = null;
+            Assert.Throws<ContractExceptionWithProperty> (() => variable.EnsuresNotNull ());
+        }
+
+        private class TestingClass { }
+    }
+}
---
+++ src/Akrual.DDD.Utils.Internal.Tests/Logging/NLogLoggerTests.cs
@@ -0,0 +1,43 @@
+﻿using Akrual.DDD.Utils.Internal.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using Xunit;
+
+namespace Akrual.DDD.Utils.Internal.Tests.Logging {
+    public class NLogLoggerTests {
+        [Fact]
+        public void Log_WithLoggerConfiguredToConsole_PrintOnConsole () {
+            var mockTarget = new MockLogTarget ();
+            ConfigureNLogConsole (mockTarget);
+            LogProvider.SetCurrentLogProvider (null);
+            var logger = LogProvider.GetCurrentClassLogger ();
+
+            logger.Debug ("Debug message");
+            Assert.Equal ("Debug message", mockTarget.LastLog.Message);
+
+            logger.Fatal ("Fatal message");
+            Assert.Equal ("Fatal message", mockTarget.LastLog.Message);
+        }
+
+        private static void ConfigureNLogConsole (Target target) {
+            var config = new LoggingConfiguration ();
+
+            config.AddTarget ("Console", target);
+
+            var dbRule = new LoggingRule ("*", NLog.LogLevel.Trace, target);
+
+            config.LoggingRules.Add (dbRule);
+
+            LogManager.Configuration = config;
+        }
+
+        internal class MockLogTarget : Target {
+            public LogEventInfo LastLog { get; set; }
+            protected override void Write (LogEventInfo logEvent) {
+                LastLog = logEvent;
+                base.Write (logEvent);
+            }
+        }
+    }
+}
